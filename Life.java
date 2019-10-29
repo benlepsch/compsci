d54453c56409c632d4496be59a54f517ce78d2eb
ben <blepsch@gmail.com>
2019-10-29 13:22:17 -0400

    save life in case i need to revert again

--- Life.java
+++ Life.java
@@ -4,10 +4,19 @@ public class Life {
     public static Shortcuts s = new Shortcuts();
     public static Life l = new Life();
 
-
+    // 2d arrays to hold values of the grid
+    // 0 = dead 1 = alive (but when printing, alive cells are printed as X)
     int[][] grid = new int[3][3];
     int[][] grid2 = new int[3][3];
 
+    // display a 2d array formatted like this:
+    /*
+        [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+        ]
+    */
     void printGrid(int[][] grid) {
         s.prntln("[");
         for (int i = 0; i < grid.length; i++) {
@@ -20,6 +29,7 @@ public class Life {
         s.prntln("]");
     }
 
+    // get number of alive neighbors to a cell
     int aliveNeighbors(int x, int y) {
         // 0 0 0
         // X X X
@@ -30,6 +40,9 @@ public class Life {
         int maxX = l.grid.length - 1;
         int maxY = l.grid[0].length - 1;
         
+        /*
+            note that the "left" "above" "above_left" etc are relative when its being displayed by showGrid() not printGrid()
+        */
         if (y > 0) {
             if (l.grid[y - 1][x] == 1) {
                 alive ++;
@@ -91,10 +104,16 @@ public class Life {
         return alive;
     }
 
+    // display the grid for the next day
+    /* looks like
+        0 0 0 
+        X X X
+        0 0 0
+    */
     void showGrid() {
         for (int y = 0; y < l.grid.length; y++) {
             for (int x = 0; x < l.grid[0].length; x++) {
-                if (l.grid[x][y] == 1)
+                if (l.grid[y][x] == 1)
                     s.prnt("X ");
                 else
                     s.prnt("0 ");
@@ -103,6 +122,7 @@ public class Life {
         }
     }
 
+    // generate the next day into l.grid2, then copy l.grid2 into l.grid1
     void nextDay() {
         int al;
         for (int i = 0; i < l.grid.length; i++) {
@@ -135,10 +155,12 @@ public class Life {
     }
 
     public static void main(String[] args) {
+        // i don't actually think this is necessary
         for (int i = 0; i < l.grid.length; i++) 
             for (int j = 0; j < l.grid[0].length; j++)
                 l.grid[i][j] = 0;
         
+        // initialize grid
         int x, y;
         do {
             s.prnt("Enter x coordinate of cell (1-"+l.grid.length+"): ");
@@ -146,13 +168,15 @@ public class Life {
             s.prnt("Enter y coordinate of cell (1-"+l.grid[0].length+") (or -1 to quit): ");
             y = s.input.nextInt() - 1;
             if (y != -2)
-                l.grid[x][y] = 1;
+                l.grid[y][x] = 1;
         
         } while (y != -2);
 
         l.showGrid();
         
         String cont = "y";
+
+        // keep displaying new days until it gets some input that isn't 'y'
         do {
             s.prnt("Show next day? (y/n): ");
             cont = s.input.next();
