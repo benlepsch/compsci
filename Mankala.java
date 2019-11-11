b3ba357c7d372ac270291536a7bc0a3d057d9475
ben <blepsch@gmail.com>
2019-11-11 13:53:43 -0500

    change mankala program to not have the game loop in main()

--- Mankala.java
+++ Mankala.java
@@ -2,21 +2,27 @@ import java.util.*;
 
 public class Mankala {
     public static Shortcuts s = new Shortcuts();
-    public static Mankala m = new Mankala();
 
     int[] p1side = new int[6];
     int[] p2side = new int[6];
     int p1home, p2home;
 
+    public Mankala() {
+        // initialize board
+        p1side = s.fill(new int[p1side.length], 3);
+        p2side = s.fill(new int[p2side.length], 3);
+        p1home = p2home = 0;
+    }
+
     void displayBoard() {
         s.prnt("  ");
-        for (int i = 0; i < m.p2side.length; i++) 
-            s.prnt(m.p2side[i] + "  ");
+        for (int i = 0; i < p2side.length; i++) 
+            s.prnt(p2side[i] + "  ");
         s.prntln();
-        s.prntln(m.p2home + "                  " + m.p1home);
+        s.prntln(p2home + "                  " + p1home);
         s.prnt("  ");
-        for (int i = 0; i < m.p1side.length; i++)
-            s.prnt(m.p1side[i] + "  ");
+        for (int i = 0; i < p1side.length; i++)
+            s.prnt(p1side[i] + "  ");
         s.prntln();
         s.prntln("  A  B  C  D  E  F");
     }
@@ -24,20 +30,20 @@ public class Mankala {
     // handle player 1's move
     // return 1 if they get to go again
     int p1move(int choice) {
-        int stones = m.p1side[choice];
-        m.p1side[choice] = 0;
+        int stones = p1side[choice];
+        p1side[choice] = 0;
 
         do {
             // fill the player's side
-            for (int i = choice + 1; i < m.p1side.length; i++) {
+            for (int i = choice + 1; i < p1side.length; i++) {
                 if (stones > 0) {
-                    m.p1side[i] ++;
+                    p1side[i] ++;
                     stones --;
 
                     // "steal" the stones from the other side and this side if it lands in an empty bin
-                    if (stones == 0 && m.p1side[i] == 1 && m.p2side[i] > 0) {
-                        m.p1home += m.p1side[i] + m.p2side[i];
-                        m.p1side[i] = m.p2side[i] = 0;
+                    if (stones == 0 && p1side[i] == 1 && p2side[i] > 0) {
+                        p1home += p1side[i] + p2side[i];
+                        p1side[i] = p2side[i] = 0;
                     }
                 } else {
                     return 0;
@@ -46,7 +52,7 @@ public class Mankala {
 
             // fill player's home bin
             if (stones > 0) {
-                m.p1home ++;
+                p1home ++;
                 stones --;
             } else 
                 return 0;
@@ -56,10 +62,10 @@ public class Mankala {
                 return 1;
             
             // fill other player's side
-            for (int i = (m.p2side.length - 1); i >= 0; i--) {
+            for (int i = (p2side.length - 1); i >= 0; i--) {
                 if (stones > 0) {
                     stones --;
-                    m.p2side[i] ++;
+                    p2side[i] ++;
                 } else {
                     return 0;
                 }
@@ -75,20 +81,20 @@ public class Mankala {
     // handle player 2's move
     // return 1 if they get to go again
     int p2move(int choice) {
-        int stones = m.p2side[choice];
-        m.p2side[choice] = 0;
+        int stones = p2side[choice];
+        p2side[choice] = 0;
 
         do {
             // fill the player's side
             for (int i = choice - 1; i >= 0; i--) {
                 if (stones > 0) {
-                    m.p2side[i] ++;
+                    p2side[i] ++;
                     stones --;
 
                     // do the steal function
-                    if (stones == 0 && m.p2side[i] == 1 && m.p1side[i] > 0) {
-                        m.p2home += m.p2side[i] + m.p1side[i];
-                        m.p2side[i] = m.p1side[i] = 0;
+                    if (stones == 0 && p2side[i] == 1 && p1side[i] > 0) {
+                        p2home += p2side[i] + p1side[i];
+                        p2side[i] = p1side[i] = 0;
                     }
                 } else 
                     return 0;
@@ -96,7 +102,7 @@ public class Mankala {
 
             // fill player's home bin
             if (stones > 0) {
-                m.p2home ++;
+                p2home ++;
                 stones --;
             } else 
                 return 0;
@@ -106,15 +112,15 @@ public class Mankala {
                 return 1;
             
             // fill other player's side
-            for (int i = 0; i < m.p1side.length; i++) {
+            for (int i = 0; i < p1side.length; i++) {
                 if (stones > 0) {
-                    m.p1side[i] ++;
+                    p1side[i] ++;
                     stones --;
                 } else 
                     return 0;
             }
 
-            choice = m.p2side.length;
+            choice = p2side.length;
 
         } while (stones > 0);
 
@@ -123,40 +129,36 @@ public class Mankala {
 
     // calculate who won
     void endGame() {
-        for (int i = 0; i < m.p1side.length; i++) {
-            m.p1home += m.p1side[i];
-            m.p2home += m.p2side[i];
+        for (int i = 0; i < p1side.length; i++) {
+            p1home += p1side[i];
+            p2home += p2side[i];
         }
-        if (m.p1home > m.p2home) {
+        if (p1home > p2home) {
             s.prntln("player 1 wins!");
             return;
         }
-        if (m.p2home > m.p1home) {
+        if (p2home > p1home) {
             s.prntln("player 2 wins!");
             return;
         }
         s.prntln("tie!");
     }
 
-    public static void main(String[] args) {
+    public void play() {
         String[] options = new String[] {"a", "b", "c", "d", "e", "f"};
         String in;
         int selected;
 
-        // initialize board
-        m.p1side = s.fill(new int[m.p1side.length], 3);
-        m.p2side = s.fill(new int[m.p2side.length], 3);
-        m.p1home = m.p2home = 0;
 
         // game loop
         while (true) {
             do {
-                m.displayBoard();
+                displayBoard();
 
                 // if player 1 doesn't have any stones
-                if (s.arrEquals(s.fill(new int[6], 0), m.p1side)) {
+                if (s.arrEquals(s.fill(new int[6], 0), p1side)) {
                     s.prntln("player 1 can't move");
-                    m.endGame();
+                    endGame();
                     return;
                 }
 
@@ -171,22 +173,22 @@ public class Mankala {
                     }
                     
                     selected = s.index(options, in.toLowerCase());
-                    if (m.p1side[selected] == 0) {
+                    if (p1side[selected] == 0) {
                         s.prntln("You don't have anything in that pit");
                         continue;
                     }
 
                     break;
                 } while (true);
-            } while (m.p1move(selected) == 1); // returns 1 if the player gets to move again, 0 if not
+            } while (p1move(selected) == 1); // returns 1 if the player gets to move again, 0 if not
 
             do {
-                m.displayBoard();
+                displayBoard();
 
                 // if player 2 doesn't have any stones
-                if (s.arrEquals(s.fill(new int[6], 0), m.p2side)) {
+                if (s.arrEquals(s.fill(new int[6], 0), p2side)) {
                     s.prntln("player 2 can't move");
-                    m.endGame();
+                    endGame();
                     return;
                 }
 
@@ -201,14 +203,19 @@ public class Mankala {
                     }
                     
                     selected = s.index(options, in.toLowerCase());
-                    if (m.p2side[selected] == 0) {
+                    if (p2side[selected] == 0) {
                         s.prntln("You don't have anything in that pit");
                         continue;
                     }
 
                     break;
                 } while (true);
-            } while (m.p2move(selected) == 1); // returns 1 if the player gets to move again, 0 if not
+            } while (p2move(selected) == 1); // returns 1 if the player gets to move again, 0 if not
         }
+    }
+
+    public static void main(String[] args) {
+        Mankala m = new Mankala();
+        m.play();
     }
 }
