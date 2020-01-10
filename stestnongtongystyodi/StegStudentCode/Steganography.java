5e682ea3349c3032780e3323b49ca565d734d69a
ben <blepsch@gmail.com>
2020-01-10 13:35:46 -0500

    working steg stuf

--- stestnongtongystyodi/StegStudentCode/Steganography.java
+++ stestnongtongystyodi/StegStudentCode/Steganography.java
@@ -52,12 +52,73 @@ public class Steganography {
         return copy;
     }
 
+    public static boolean canHide(Picture source, Picture secret) {
+        Pixel[][] sourcePix = source.getPixels2D();
+        Pixel[][] secretPix = secret.getPixels2D();
+
+        if (sourcePix.length == secretPix.length && sourcePix[0].length == secretPix[0].length) {
+            return true;
+        }
+        return false;
+    }
+
+    public static Picture hidePicture(Picture source, Picture secret) {
+        Picture copy = new Picture(source);
+        Pixel[][] pixels = copy.getPixels2D();
+        Pixel[][] secretPix = secret.getPixels2D();
+
+        for (int i = 0; i < secretPix.length; i++) {
+            for (int j = 0; j < secretPix[0].length; j++) {
+                Color col = secretPix[i][j].getColor();
+                setLow(pixels[i][j], col);
+            }
+        }
+
+        return copy;
+    }
+
+    public static Picture hidePicture(Picture source, Picture secret, int startRow, int startCol) {
+        Picture copy = new Picture(source);
+        Pixel[][] pixels = copy.getPixels2D();
+        Pixel[][] secretPix = secret.getPixels2D();
+
+        for (int i = 0; i < secretPix.length; i++) {
+            for (int j = 0; j < secretPix[0].length; j++) {
+                Color col = secretPix[i][j].getColor();
+                setLow(pixels[i + startRow][j + startCol], col);
+            }
+        }
+
+        return copy;
+    }
+
+    public static boolean isSame(Picture pic1, Picture pic2) {
+        Pixel[][] p1 = pic1.getPixels2D();
+        Pixel[][] p2 = pic2.getPixels2D();
+
+        if (p1.length != p2.length || p1[0].length != p2[0].length) {
+            return false;
+        }
+
+        for (int i = 0; i < p1.length; i++) {
+            for (int j = 0; j < p1[0].length; j++) {
+                Color c1 = p1[i][j].getColor();
+                Color c2 = p2[i][j].getColor();
+
+                if (c1.getRed() != c2.getRed() || c1.getBlue() != c2.getBlue() || c1.getGreen() != c2.getGreen()) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        Picture beach = new Picture("beach.jpg");
-        beach.explore();
-        testSetLow(beach, Color.PINK);
-        beach.explore();
-        Picture bruh = revealPicture(beach);
-        bruh.explore();
+        Picture swan = new Picture("swan.jpg");
+        Picture swan2 = new Picture("swan.jpg");
+        System.out.println("Swan and swan2 are the same: " + isSame(swan, swan2));
+        testClearLow(swan);
+        System.out.println("Swan aaaaa: " + isSame(swan, swan2));
     }
 }
